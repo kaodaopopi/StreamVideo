b51d36833d3d206ff55ae84a01fd93199a4223da
Kao <66681691+kaodaopopi@users.noreply.github.com>
2020-10-10 21:37:13 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace StreamVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Camera";
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(106, 26);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(121, 20);
+            this.cboCamera.TabIndex = 7;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(654, 29);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(25, 96);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(638, 372);
+            this.pic.TabIndex = 9;
+            this.pic.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(226, 500);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "StreamPicture";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(965, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ReceivePicture";
+            // 
+            // pic2
+            // 
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(738, 96);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(638, 372);
+            this.pic2.TabIndex = 13;
+            this.pic2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 583);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cboCamera);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pic2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.FFMPEG;
+using System.IO;
+using System.Net;
+using System.Drawing.Imaging;
+
+namespace StreamVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Camera parameters
+        private VideoCaptureDevice Camera = null;
+        //Encode each frame of image to video file
+        private VideoFileWriter videofilewriter = new VideoFileWriter();
+        //Image cache
+        private Bitmap bmp;
+        private Bitmap bmpRe;
+
+        public byte[] byteImage;
+        public byte[] byteImage_Back;
+
+        FilterInfoCollection filterInfoCollection;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Camera.IsRunning == true)
+                //Stop camera
+                Camera.Stop();
+                //Close the video file, if you forget to close it, you will get a damaged file that cannot be played
+                videofilewriter.Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Camera = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            Camera.NewFrame += VideoCaptureDevice_NewFrame;
+            Camera.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Image = (Bitmap)eventArgs.Frame.Clone();
+            bmp = (Bitmap)eventArgs.Frame.Clone();
+
+            MemoryStream memoryStream = new MemoryStream();
+            bmp.Save(memoryStream, ImageFormat.Bmp);
+            byteImage = new Byte[memoryStream.Length];
+            byteImage = memoryStream.ToArray();
+
+            //Required API
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("Your URL");
+            //Requested http verb
+            request.Method = "POST";
+            //Requested content type
+            request.ContentType = "video/mp4";
+            //Convert the data content to be post to stream
+            using (Stream streamWriter = request.GetRequestStream())
+            {
+                streamWriter.Write(byteImage, 0, byteImage.Length);
+            }
+            //Send Request
+            using (WebResponse response = request.GetResponse())
+            {
+                using (MemoryStream getmemory = new MemoryStream())
+                {
+                    response.GetResponseStream().CopyTo(getmemory);
+                    byteImage_Back = getmemory.ToArray();
+
+                    using (MemoryStream ms = new MemoryStream(byteImage_Back))
+                    {
+                        bmpRe = new Bitmap(ms);
+                        pic2.Image = (Bitmap)bmpRe.Clone();
+                    }
+                }//end using  
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+            Camera = new VideoCaptureDevice();
+        }
+    }
+}
